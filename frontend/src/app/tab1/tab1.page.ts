29de8093ace32982e05f595990f8010ad5bd6fe7
awesome.rainbows7@gmail.com <awesome.rainbows7@gmail.com>
2020-12-18 22:35:58 +0900

    create accodion

--- frontend/src/app/tab1/tab1.page.ts
+++ frontend/src/app/tab1/tab1.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  public items: any = [];
 
-  constructor() {}
+  constructor() {
+    this.items = [
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false }
+    ];
+  }
 
+  expandItem(item): void {
+    if (item.expanded) {
+      item.expanded = false;
+    } else {
+      this.items.map(listItem => {
+        if (item == listItem) {
+          listItem.expanded = !listItem.expanded;
+        } else {
+          listItem.expanded = false;
+        }
+        return listItem;
+      });
+    }
+  }
 }
